6454434322f1ab01f9faa449100ae3540e2a9f2d
Oleksii Koshelenko <KOO2LE@bosch.com>
2025-05-08 18:21:24 +0300

    turn on resume

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ import Container from "react-bootstrap/Container";
 import logo from "../Assets/logo.png";
 import { Link } from "react-router-dom";
 import { AiOutlineHome, AiOutlineFundProjectionScreen, AiOutlineUser } from "react-icons/ai";
-// import { CgFileDocument } from "react-icons/cg";
+import { CgFileDocument } from "react-icons/cg";
 import { useTranslation } from 'react-i18next';
 import flagEN from '../Assets/flagEN.png'; 
 import flagUA from '../Assets/flagUA.png'; 
